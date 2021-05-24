806ebdb0dcd9b7b53ed6ec1384264764ae16f44d
caraanJC <caraanjohncarlo@gmail.com>
2021-05-25 00:47:22 +0800

    feat: add Navbar UI

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,33 @@
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar__logo'>
+        <h2>MERN Shopping Cart</h2>
+      </div>
+
+      <ul className='navbar__links'>
+        <li>
+          <Link to='/cart' className='cart__link'>
+            <i className='fas fa-shopping-cart'></i>
+            <span>
+              Cart
+              <span className='cartlogo__badge'>0</span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to='/cart'>Shop</Link>
+        </li>
+      </ul>
+
+      <div className='hamburger__menu'>
+        <i className='fas fa-bars'></i>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
